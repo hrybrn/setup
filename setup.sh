a2ff4d0f08062b64108596f14b636e79658592ce
hbrown3 <harry_brown@intuit.com>
2020-11-13 12:25:28 +0000

    reorder setup script

--- setup.sh
+++ setup.sh
@@ -17,6 +17,14 @@ then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+read -p "do you want to install development programs? (requires brew) y/n " -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  brew install vim zsh wget htop hub thefuck neovim swiftlint zlib yarn wget
+  brew cask install google-chrome visual-studio-code intellij-idea spotify pock karabiner-elements alfred firefox iterm2 amethyst gitkraken postman via
+fi
+
 read -p "do you want to install sdkman? y/n " -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -31,29 +39,18 @@ then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 fi
 
+read -p "do you want to install rvm? y/n " -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  curl -sSL https://get.rvm.io | bash -s stable
+fi
+
 read -p "do you want to install pyenv? y/n " -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   curl https://pyenv.run | bash
-fi
-
-read -p "do you want to install docker? (requires brew) y/n " -n 1 -r
-echo 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
-  brew cask install kitematic
-  sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-  sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-fi
-
-read -p "do you want to install development programs? (requires brew) y/n " -n 1 -r
-echo 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  brew install vim zsh wget htop
-  brew cask install google-chrome visual-studio-code intellij-idea spotify hyperswitch pock karabiner-elements alfred firefox iterm2
 fi
 
 read -p "do you want to generate a ssh key? y/n " -n 1 -r
@@ -70,3 +67,13 @@ then
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   cp $DIR/.zshrc ~/.zshrc
 fi
+
+echo "here are a few things that you might want to do next:
+  - node
+  - python
+    - cairosvg
+  - ruby
+    - bundle
+  - docker
+  - git profile
+"
